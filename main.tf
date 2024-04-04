32674f7b900b2a6fa4fc235fcaaa50210b9e1a77
crunch84526 <165909596+crunch84526@users.noreply.github.com>
2024-04-04 12:25:32 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
+# Définition du provider Azure
+provider "azurerm" {
+  features {}
+}
+
+# Référencement du groupe de ressources existant
+data "azurerm_resource_group" "existing" {
+  name = "azurerm_resource_group"
+}
+
 
