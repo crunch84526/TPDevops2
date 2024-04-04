6fecd9ee5c3fb91ff4ee9d39f7e3deb4f47416be
crunch84526 <165909596+crunch84526@users.noreply.github.com>
2024-04-04 12:29:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,10 @@ provider "azurerm" {
   features {}
 }
 
-# Référencement du groupe de ressources existant
-data "azurerm_resource_group" "existing" {
-  name = "azurerm_resource_group"
+# Création du groupe de ressources
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "East US"
 }
 
 
