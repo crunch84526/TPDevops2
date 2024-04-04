0b37a003307fa9f3e55b03c7e2bc82a0f836865e
crunch84526 <165909596+crunch84526@users.noreply.github.com>
2024-04-04 12:30:27 +0200

    Update sa.tf

--- sa.tf
+++ sa.tf
@@ -1,1 +1,17 @@
+# Référencement du groupe de ressources existant
+data "azurerm_resource_group" "existing" {
+  name = azurerm_resource_group.example.name
+}
 
+# Création du compte de stockage
+resource "azurerm_storage_account" "example" {
+  name                     = "azurerm_storage_account"
+  resource_group_name      = data.azurerm_resource_group.existing.name
+  location                 = data.azurerm_resource_group.existing.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = "dev"
+  }
+}
